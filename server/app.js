61d3d39bfb5452d7588dee978af1841fb7103a80
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-10 08:39:27 +0500

    implement basic Express server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,12 @@
-console.log("serer is running");
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
